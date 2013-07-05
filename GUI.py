1ff6aa8597c65085088f0a81020ef77b60f6a29c
Ricardo Garcia <commits@rg3.name>
2013-07-05 19:36:14 +0200

    Connect GUI to the storage backend for entry operations
    Bookmark operations still missing.

--- GUI.py
+++ GUI.py
@@ -21,6 +21,7 @@ import ttk
 import tkFont
 import tkMessageBox
 import os.path
+from datetime import datetime
 
 # Our modules.
 import StorageBackend
@@ -135,9 +136,121 @@ class DefaultGUI(object):
         **PADEXPAND )
 
     IMAGE_PATH = os.path.join(os.path.dirname(__file__), 'pixmap')
+    TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+    def _is_valid_entry_index(self, index):
+        return (index is not None and
+                index >= 0 and index < len(self._entries_list))
+
+    def _entry_listbox_select_handler(self, *args):
+        selection = self.entry_listbox.curselection()
+        if len(selection) <= 0:
+            return
+        sel = selection[0]
+        if not self._is_valid_entry_index(sel):
+            return
+        self._do_change_entry(sel)
+
+    def _select_entry_in_listbox(self, index):
+        self.entry_listbox.selection_clear(0, 'end')
+        self.entry_listbox.selection_set(index)
+
+    def _do_change_entry(self, index):
+        if index == self._current_entry_index:
+            return
+
+        if self.entry_text.edit_modified():
+            if tkMessageBox.askyesno(
+                "%s: Current entry modified" % WINDOW_CLASS,
+                "Save current entry?"):
+                self._save_button_handler()
+
+        if self._is_valid_entry_index(index):
+            log_entry = self._entries_list[index]
+            if log_entry.data is None:
+                log_entry = self._backend.get_entry(log_entry.id_le)
+            self._current_entry_index = index
+            ctime = log_entry.ctime.strftime(self.TIME_FORMAT)
+            mtime = log_entry.mtime.strftime(self.TIME_FORMAT)
+            self.ctime_value_label['text'] = ctime
+            self.mtime_value_label['text'] = mtime
+            self.entry_text.delete('0.0', 'end')
+            self.entry_text['state'] = 'normal'
+            if log_entry.data is not None:
+                self.entry_text.insert('0.0', log_entry.data)
+            self._select_entry_in_listbox(index)
+            self.save_button['state'] = 'enabled'
+        else:
+            self._current_entry_index = None
+            self.ctime_value_label['text'] = ''
+            self.mtime_value_label['text'] = ''
+            self.entry_text.delete('0.0', 'end')
+            self.entry_text['state'] = 'disabled'
+            self.save_button['state'] = 'disabled'
+
+        self.entry_text.edit_modified(False)
+
+    def _update_entry_list(self):
+        current_entry_id = (None if self._current_entry_index is None else
+                            self._entries_list[self._current_entry_index].id_le)
+
+        self._entries_list = self._backend.list_entries()
+        self._entries_list.sort(reverse=True)
+        self.entry_listbox.delete(0, 'end')
+        for e in self._entries_list:
+            self.entry_listbox.insert('end', e.ctime.strftime(self.TIME_FORMAT))
+
+        pos = None
+        if current_entry_id is not None:
+            # Find current entry in list.
+            positions = [x for x in xrange(len(self._entries_list))
+                         if self._entries_list[x].id_le == current_entry_id]
+            if len(positions) > 0:
+                pos = positions[0]
+        self._do_change_entry(pos)
+
+    def _get_current_entry(self):
+        if not self._is_valid_entry_index(self._current_entry_index):
+            return None
+        return self._entries_list[self._current_entry_index]
+
+    def _save_button_handler(self, *args):
+        entry = self._get_current_entry()
+        if entry is None:
+            return
+        entry.data = self.entry_text.get('0.0', 'end')
+        entry.mtime = datetime.utcnow()
+        self.save_button['state'] = 'disabled'
+        self._backend.save_entry(entry)
+        self.save_button['state'] = 'enabled'
+        mtime_text = entry.mtime.strftime(self.TIME_FORMAT)
+        self.mtime_value_label['text'] = mtime_text
+        self.entry_text.edit_modified(False)
+
+    def _new_entry_button_handler(self, *args):
+        new_entry = self._backend.new_entry()
+        self._entries_list[:0] = [new_entry]
+        if self._current_entry_index is not None:
+            self._current_entry_index += 1
+        self.entry_listbox.insert(0, new_entry.ctime.strftime(self.TIME_FORMAT))
+        self._do_change_entry(0)
+
+    def _del_entry_button_handler(self, *args):
+        entry = self._get_current_entry()
+        if entry is None:
+            return
+        if tkMessageBox.askyesno(
+                "%s: Confirm deletion",
+                "Are you sure you want to delete this entry?"):
+            # Mark as not modified to avoid being asked to save entry.
+            self.entry_text.edit_modified(False)
+            self._backend.del_entry(entry.id_le)
+            self._update_entry_list()
 
     def __init__(self, backend):
         self._backend = backend
+        self._entries_list = []
+        self._current_entry_index = None
 
         #
         # Widgets.
@@ -172,18 +285,18 @@ class DefaultGUI(object):
         # Entries tab.
         self.new_entry_button = ttk.Button(self.entries_frame, text='New Entry',
                                            image=self.new_img,
-                                           compound=self.DEFAULT_COMPOUND)
+                                           compound=self.DEFAULT_COMPOUND,
+                                           command=self._new_entry_button_handler)
         self.del_entry_button = ttk.Button(self.entries_frame,
                                            text='Delete Entry',
                                            image=self.del_img,
-                                           compound=self.DEFAULT_COMPOUND)
+                                           compound=self.DEFAULT_COMPOUND,
+                                           command=self._del_entry_button_handler)
         self.entry_list_frame = ttk.Button(self.entries_frame)
         self.entry_listbox = Tkinter.Listbox(self.entry_list_frame,
                                              height=self.DEFAULT_LISTBOX_HEIGHT)
-        # XXX
-        for i in [x for x in xrange(1, 30+1)][::-1]:
-            self.entry_listbox.insert('end', 'Entry from 2013-06-%02d' % i)
-
+        self.entry_listbox.bind('<<ListboxSelect>>',
+                                self._entry_listbox_select_handler)
         self.entry_listbox_sb = ttk.Scrollbar(self.entry_list_frame,
                                               command=self.entry_listbox.yview)
         self.entry_listbox['yscrollcommand'] = self.entry_listbox_sb.set
@@ -200,11 +313,6 @@ class DefaultGUI(object):
         self.bookmark_list_frame = ttk.Button(self.bookmarks_frame)
         self.bookmark_listbox = Tkinter.Listbox(
                 self.bookmark_list_frame, height=self.DEFAULT_LISTBOX_HEIGHT)
-
-        # XXX
-        for i in xrange(1, 30+1):
-            self.bookmark_listbox.insert('end', 'Bookmark number %s' % i)
-
         self.bookmark_listbox_sb = ttk.Scrollbar(
                 self.bookmark_list_frame, command=self.bookmark_listbox.yview)
         self.bookmark_listbox['yscrollcommand'] = self.bookmark_listbox_sb.set
@@ -220,14 +328,14 @@ class DefaultGUI(object):
         self.mtime_value_label = ttk.Label(self.entry_display_frame)
         self.save_button = ttk.Button(self.entry_display_frame,
                                       text='Save Entry', image=self.save_img,
-                                      compound=self.DEFAULT_COMPOUND)
+                                      compound=self.DEFAULT_COMPOUND,
+                                      command=self._save_button_handler)
         self.save_button['state'] = 'disabled'
         self.entry_text_sep = ttk.Separator(self.entry_display_frame,
                                             orient='horizontal')
         self.entry_text_frame = ttk.Frame(self.entry_display_frame)
         self.entry_text = Tkinter.Text(self.entry_text_frame, width=80,
                                        wrap='word', relief='solid')
-        #entry_text.insert('0.0', lorem_ipsum)
         self.entry_text_sb = ttk.Scrollbar(self.entry_text_frame,
                                            command=self.entry_text.yview)
         self.entry_text['yscrollcommand'] = self.entry_text_sb.set
@@ -301,9 +409,12 @@ class DefaultGUI(object):
         self.exit_button.grid(row=1, column=0, columnspan=3,
                               pady=(self.DEFAULT_PADDING, 0), sticky=Tkinter.E)
 
-        style = ttk.Style()
+        #style = ttk.Style()
         #s.theme_use('alt')
-        style.theme_use('clam')
+        #style.theme_use('clam')
+
+        self._update_entry_list()
+        self._do_change_entry(None)
 
     def mainloop(self):
         self.root.mainloop()
