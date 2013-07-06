3b3ab0d4c0a4bfbd2a53d88c8b3e4f82c986e728
Ricardo Garcia <commits@rg3.name>
2013-07-06 13:57:07 +0200

    Create missing connections from GUI to backend

--- GUI.py
+++ GUI.py
@@ -20,6 +20,7 @@ import Tkinter
 import ttk
 import tkFont
 import tkMessageBox
+import tkSimpleDialog
 import os.path
 from datetime import datetime
 
@@ -138,25 +139,45 @@ class DefaultGUI(object):
     IMAGE_PATH = os.path.join(os.path.dirname(__file__), 'pixmap')
     TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
-    def _is_valid_entry_index(self, index):
-        return (index is not None and
-                index >= 0 and index < len(self._entries_list))
+    def _is_valid_list_index(self, i, lst):
+        return (i is not None and i >= 0 and i < len(lst))
 
-    def _entry_listbox_select_handler(self, *args):
-        selection = self.entry_listbox.curselection()
-        if len(selection) <= 0:
-            return
-        sel = selection[0]
-        if not self._is_valid_entry_index(sel):
-            return
-        self._do_change_entry(sel)
+    def _is_valid_entry_index(self, index):
+        return self._is_valid_list_index(index, self._entries_list)
+
+    def _is_valid_bookmark_index(self, index):
+        return self._is_valid_list_index(index, self._bookmarks_list)
 
     def _select_entry_in_listbox(self, index):
         self.entry_listbox.selection_clear(0, 'end')
-        self.entry_listbox.selection_set(index)
+        if index is not None:
+            self.entry_listbox.selection_set(index)
+        self._current_entry_index = index
+
+    def _select_bookmark_in_listbox(self, index):
+        self.bookmark_listbox.selection_clear(0, 'end')
+        if index is not None:
+            self.bookmark_listbox.selection_set(index)
+        self._current_bookmark_index = index
+
+    def _do_select_bookmark(self, index):
+        if (self._is_valid_bookmark_index(index) and
+            index == self._current_bookmark_index):
+            return
+
+        if self._is_valid_bookmark_index(index):
+            entry_index = self._entry_index_by_id(
+                    self._bookmarks_list[index].id_le)
+            self._do_change_entry(entry_index)
+            self.del_bookmark_button['state'] = 'enabled'
+        else:
+            self.del_bookmark_button['state'] = 'disabled'
+
+        self._select_bookmark_in_listbox(index)
 
     def _do_change_entry(self, index):
-        if index == self._current_entry_index:
+        if (self._is_valid_entry_index(index) and
+            index == self._current_entry_index):
             return
 
         if self.entry_text.edit_modified():
@@ -169,7 +190,6 @@ class DefaultGUI(object):
             log_entry = self._entries_list[index]
             if log_entry.data is None:
                 log_entry = self._backend.get_entry(log_entry.id_le)
-            self._current_entry_index = index
             ctime = log_entry.ctime.strftime(self.TIME_FORMAT)
             mtime = log_entry.mtime.strftime(self.TIME_FORMAT)
             self.ctime_value_label['text'] = ctime
@@ -178,17 +198,37 @@ class DefaultGUI(object):
             self.entry_text['state'] = 'normal'
             if log_entry.data is not None:
                 self.entry_text.insert('0.0', log_entry.data)
-            self._select_entry_in_listbox(index)
             self.save_button['state'] = 'enabled'
+            self.new_bookmark_button['state'] = 'enabled'
+            self.del_entry_button['state'] = 'enabled'
+            self.entry_text.focus()
         else:
-            self._current_entry_index = None
             self.ctime_value_label['text'] = ''
             self.mtime_value_label['text'] = ''
             self.entry_text.delete('0.0', 'end')
             self.entry_text['state'] = 'disabled'
             self.save_button['state'] = 'disabled'
+            self.new_bookmark_button['state'] = 'disabled'
+            self.del_entry_button['state'] = 'disabled'
 
+        self._select_entry_in_listbox(index)
         self.entry_text.edit_modified(False)
+
+    def _entry_index_by_id(self, entry_id):
+        if entry_id is None:
+            return None
+
+        positions = [x for x in xrange(len(self._entries_list))
+                     if self._entries_list[x].id_le == entry_id]
+        return (None if len(positions) <= 0 else positions[0])
+
+    def _bookmark_index_by_id(self, bookmark_id):
+        if bookmark_id is None:
+            return None
+
+        positions = [x for x in xrange(len(self._bookmarks_list))
+                     if self._bookmarks_list[x].id_bm == bookmark_id]
+        return (None if len(positions) <= 0 else positions[0])
 
     def _update_entry_list(self):
         current_entry_id = (None if self._current_entry_index is None else
@@ -200,14 +240,22 @@ class DefaultGUI(object):
         for e in self._entries_list:
             self.entry_listbox.insert('end', e.ctime.strftime(self.TIME_FORMAT))
 
-        pos = None
-        if current_entry_id is not None:
-            # Find current entry in list.
-            positions = [x for x in xrange(len(self._entries_list))
-                         if self._entries_list[x].id_le == current_entry_id]
-            if len(positions) > 0:
-                pos = positions[0]
+        pos = self._entry_index_by_id(current_entry_id)
         self._do_change_entry(pos)
+
+    def _update_bookmarks_list(self):
+        current_bookmark_id = (None if self._current_bookmark_index is None else
+            self._bookmarks_list[self._current_bookmark_index].id_bm)
+
+        self._bookmarks_list = self._backend.list_bookmarks()
+        self._bookmarks_list.sort()
+
+        self.bookmark_listbox.delete(0, 'end')
+        for b in self._bookmarks_list:
+            self.bookmark_listbox.insert('end', b.text)
+
+        pos = self._bookmark_index_by_id(current_bookmark_id)
+        self._do_select_bookmark(pos)
 
     def _get_current_entry(self):
         if not self._is_valid_entry_index(self._current_entry_index):
@@ -246,11 +294,55 @@ class DefaultGUI(object):
             self.entry_text.edit_modified(False)
             self._backend.del_entry(entry.id_le)
             self._update_entry_list()
+            self._update_bookmarks_list()
+
+    def _new_bookmark_button_handler(self, *args):
+        entry = self._get_current_entry()
+        if entry is None:
+            return
+        text = tkSimpleDialog.askstring(
+                "%s: New Bookmark" % WINDOW_CLASS, "Bookmark name:")
+        if text is None:
+            return
+        bookmark = self._backend.new_bookmark(entry.id_le, text)
+        self._update_bookmarks_list()
+        pos = self._bookmark_index_by_id(bookmark.id_bm)
+        self._do_select_bookmark(pos)
+
+    def _del_bookmark_button_handler(self, *args):
+        if not self._is_valid_bookmark_index(self._current_bookmark_index):
+            return
+        bookmark = self._bookmarks_list[self._current_bookmark_index]
+        self._backend.del_bookmark(bookmark.id_bm)
+        self._do_select_bookmark(None)
+        self._update_bookmarks_list()
+
+    def _entry_listbox_select_handler(self, *args):
+        selection = self.entry_listbox.curselection()
+        if len(selection) <= 0:
+            return
+        sel = selection[0]
+        if not self._is_valid_entry_index(sel):
+            return
+        # Selected bookmark no longer valid after this.
+        self._current_bookmark_index = None
+        self._do_change_entry(sel)
+
+    def _bookmark_listbox_select_handler(self, *args):
+        selection = self.bookmark_listbox.curselection()
+        if len(selection) <= 0:
+            return
+        sel = selection[0]
+        if not self._is_valid_bookmark_index(sel):
+            return
+        self._do_select_bookmark(sel)
 
     def __init__(self, backend):
         self._backend = backend
         self._entries_list = []
+        self._bookmarks_list = []
         self._current_entry_index = None
+        self._current_bookmark_index = None
 
         #
         # Widgets.
@@ -283,15 +375,14 @@ class DefaultGUI(object):
         self.notebook.add(self.bookmarks_frame, text='Bookmarks')
 
         # Entries tab.
-        self.new_entry_button = ttk.Button(self.entries_frame, text='New Entry',
-                                           image=self.new_img,
-                                           compound=self.DEFAULT_COMPOUND,
-                                           command=self._new_entry_button_handler)
-        self.del_entry_button = ttk.Button(self.entries_frame,
-                                           text='Delete Entry',
-                                           image=self.del_img,
-                                           compound=self.DEFAULT_COMPOUND,
-                                           command=self._del_entry_button_handler)
+        self.new_entry_button = ttk.Button(
+                self.entries_frame, text='New Entry', image=self.new_img,
+                compound=self.DEFAULT_COMPOUND,
+                command=self._new_entry_button_handler)
+        self.del_entry_button = ttk.Button(
+                self.entries_frame, text='Delete Entry', image=self.del_img,
+                compound=self.DEFAULT_COMPOUND,
+                command=self._del_entry_button_handler)
         self.entry_list_frame = ttk.Button(self.entries_frame)
         self.entry_listbox = Tkinter.Listbox(self.entry_list_frame,
                                              height=self.DEFAULT_LISTBOX_HEIGHT)
@@ -302,17 +393,19 @@ class DefaultGUI(object):
         self.entry_listbox['yscrollcommand'] = self.entry_listbox_sb.set
 
         # Bookmarks tab.
-        self.new_bookmark_button = ttk.Button(self.bookmarks_frame,
-                                              text='New Bookmark',
-                                              image=self.new_img,
-                                              compound=self.DEFAULT_COMPOUND)
-        self.del_bookmark_button = ttk.Button(self.bookmarks_frame,
-                                              text='Delete Bookmark',
-                                              image=self.del_img,
-                                              compound=self.DEFAULT_COMPOUND)
+        self.new_bookmark_button = ttk.Button(
+                self.bookmarks_frame, text='New Bookmark', image=self.new_img,
+                compound=self.DEFAULT_COMPOUND,
+                command=self._new_bookmark_button_handler)
+        self.del_bookmark_button = ttk.Button(
+                self.bookmarks_frame, text='Delete Bookmark',
+                image=self.del_img, compound=self.DEFAULT_COMPOUND,
+                command=self._del_bookmark_button_handler)
         self.bookmark_list_frame = ttk.Button(self.bookmarks_frame)
         self.bookmark_listbox = Tkinter.Listbox(
                 self.bookmark_list_frame, height=self.DEFAULT_LISTBOX_HEIGHT)
+        self.bookmark_listbox.bind('<<ListboxSelect>>',
+                self._bookmark_listbox_select_handler)
         self.bookmark_listbox_sb = ttk.Scrollbar(
                 self.bookmark_list_frame, command=self.bookmark_listbox.yview)
         self.bookmark_listbox['yscrollcommand'] = self.bookmark_listbox_sb.set
@@ -414,7 +507,9 @@ class DefaultGUI(object):
         #style.theme_use('clam')
 
         self._update_entry_list()
+        self._update_bookmarks_list()
         self._do_change_entry(None)
+        self._do_select_bookmark(None)
 
     def mainloop(self):
         self.root.mainloop()
@@ -425,9 +520,4 @@ def main():
     if backend is not None:
         gui = DefaultGUI(backend)
         gui.mainloop()
-
-#answer = tkMessageBox.askyesno(
-#    title='Sooperimportant Question',
-#    message='Is 'no' the answer to this question?')
-#print 'Answer: %r' % answer
 
